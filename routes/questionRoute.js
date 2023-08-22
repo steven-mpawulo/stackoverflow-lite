ebdac1cad16759a25281b0532cbdd63ab8ebc561
steven-mpawulo <stevenmpawulo@gmail.com>
2023-08-22 11:09:23 +0300

    adds fetching all questions controller

--- routes/questionRoute.js
+++ routes/questionRoute.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const addQuestion = require("../controllers/addQuestion");
 const { model } = require("mongoose");
+const getAllQuestions = require("../controllers/getAllQuestionsController");
 const questionRouter = express.Router();
 
 questionRouter.post('/questions', addQuestion);
+questionRouter.get('/questions', getAllQuestions);
 
 
 module.exports = questionRouter;
