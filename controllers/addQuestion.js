8468521d7b630c75d9040cc1e3012e899da1cc8a
steven-mpawulo <stevenmpawulo@gmail.com>
2023-08-22 10:36:02 +0300

    finishes up on adding questions funcionality

---
+++ controllers/addQuestion.js
@@ -0,0 +1,25 @@
+const questionModel = require("../models/questionModel");
+
+const addQuestion = (req, res) => {
+    const body = req.body;
+    console.log(body);
+    if (Object.keys(body).length === 0){
+        res.status(400).json({"message": "please provide some data"});
+
+    } else {
+        const question = new questionModel({
+            question: body.question,
+        });
+
+        question.save().then((value) => {
+            console.log(value);
+            res.status(200).json({"message": "question added", "question": value});
+        }).catch((e) => {
+            console.log(e);
+            res.status(400).json({"message": "something went wrong"});
+        });
+    }
+
+}
+
+module.exports = addQuestion;
