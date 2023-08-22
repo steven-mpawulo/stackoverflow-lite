ca37ba09b6a76b10f6c12d92ee6da418882093e9
steven-mpawulo <stevenmpawulo@gmail.com>
2023-08-22 11:23:47 +0300

    adds getting specific question controller

--- routes/questionRoute.js
+++ routes/questionRoute.js
@@ -2,10 +2,12 @@ const express = require("express");
 const addQuestion = require("../controllers/addQuestion");
 const { model } = require("mongoose");
 const getAllQuestions = require("../controllers/getAllQuestionsController");
+const getSpecificQuestion = require("../controllers/getSpecificQuestion");
 const questionRouter = express.Router();
 
 questionRouter.post('/questions', addQuestion);
 questionRouter.get('/questions', getAllQuestions);
+questionRouter.get('questions/:id', getSpecificQuestion);
 
 
 module.exports = questionRouter;
