a53126f4b53ec329d8cb5150e06b8f65911c9af6
steven-mpawulo <stevenmpawulo@gmail.com>
2023-08-22 14:33:47 +0300

    finishes up on deleting question controller

---
+++ controllers/deleteQuestionController.js
@@ -0,0 +1,19 @@
+const questionModel = require("../models/questionModel")
+
+const deleteQuestion = async (req, res) => {
+    const id = req.params.id;
+    if (id !== null) {
+        await questionModel.findOneAndDelete({'_id': id}).then((value) => {
+            console.log(value);
+            res.status(200).json({"message": "question deleted", "question": value});
+        }).catch((e) => {
+            console.log(e);
+            res.status(400).json({"message": "something went wrong"});
+
+        });
+    } else {
+        res.status(400).json({"message": "something went wrong"});
+    }
+    
+
+}
