d99bce238f93332350e5cecfd092c36260d9882a
steven-mpawulo <stevenmpawulo@gmail.com>
2023-08-22 10:40:37 +0300

    adds question router

--- routes/questionRoute.js
+++ routes/questionRoute.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const addQuestion = require("../controllers/addQuestion");
+const { model } = require("mongoose");
 const questionRouter = express.Router();
 
 questionRouter.post('/questions', addQuestion);
+
+
+module.exports = questionRouter;
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const questionRouter = require("./routes/questionRoute");
 require('dotenv').config();
 const app = express();
+
+app.use(express.json());
 
 const port = process.env.PORT || 5001;
 
@@ -8,7 +11,9 @@ const port = process.env.PORT || 5001;
 app.get("/", (req, res) => {
     res.json({"message": "welcome to this server"});
 
-})
+});
+
+app.use('/api/v1', questionRouter);
 app.listen(port, () => {
     console.log("Server has started");
 })
