e6905ddf8e6140a744848b4618e81b4edb8b58a4
steven-mpawulo <stevenmpawulo@gmail.com>
2023-08-22 12:09:52 +0300

    creates add answer controller

---
+++ controllers/addAnswer.js
@@ -0,0 +1,18 @@
+const questionModel = require("../models/questionModel");
+
+const addAnswer = (req, res) => {
+    const body = req.body;
+    console.log(body);
+    const id = req.params.id;
+    console.log(id);
+    if (id !== null) {
+        if (Object.keys(body).length !== 0) {
+            questionModel.findOneAndUpdate({ '_id': id }, { 'answer': body.answer });
+        } else {
+            res.status(400).json({ "message": "something went wrong" });
+        }
+
+    } else {
+        res.status(400).json({ "message": "something went wrong" });
+    }
+}
