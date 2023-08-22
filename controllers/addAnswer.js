2c3137bf04b86656860dc43aee662c9ee7374694
steven-mpawulo <stevenmpawulo@gmail.com>
2023-08-22 12:17:52 +0300

    modifies add answer controller

--- controllers/addAnswer.js
+++ controllers/addAnswer.js
@@ -7,7 +7,15 @@ const addAnswer = (req, res) => {
     console.log(id);
     if (id !== null) {
         if (Object.keys(body).length !== 0) {
-            questionModel.findOneAndUpdate({ '_id': id }, { 'answer': body.answer });
+            questionModel.findOneAndUpdate({ '_id': id }, { 'answer': body.answer }).exec().then((value) => {
+                console.log(value);
+                res.status(400).json({"message": "answer added",
+                "question": value,
+            });
+            }).catch((e) => {
+                console.log(e);
+                res.status(400).json({"message": "something went wrong"});
+            });
         } else {
             res.status(400).json({ "message": "something went wrong" });
         }
