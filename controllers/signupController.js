2d67be340b7b008ed38ed7cec1783332eefa92cc
steven-mpawulo <stevenmpawulo@gmail.com>
2023-08-23 10:23:58 +0300

    finishes up with sign up controller

--- controllers/signupController.js
+++ controllers/signupController.js
@@ -1,9 +1,30 @@
-const signup = (req, res) => {
+const bcrypt = require('bcrypt');
+const userModel = require('../models/userModel');
+const signup = async (req, res) => {
     const body = req.body;
     if (Object.keys(body).length === 0) {
         res.status(400).json({"message": "please provide some data"});
     } else {
+        const hashedPassword = await bcrypt.hash(body.password, 10);
+        const user = userModel({
+            username: body.username,
+            email: body.email,
+            password: hashedPassword,
+        });
+
+        await user.save().then((value) => {
+            console.log(value);
+            res.status(200).json({
+                "message": "accounted created",
+                "user details":value,
+            });
+        }).catch((e) => {
+            console.log(e);
+            res.status(400).json({"message": "something went wrong"});
+        });
         
     }
 
 }
+
+module.exports = signup;
