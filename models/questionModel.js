d7013e579103c3b869aa1057e1e1082b676707ca
steven-mpawulo <stevenmpawulo@gmail.com>
2023-08-23 12:00:54 +0300

    modifies question schema and model

--- models/questionModel.js
+++ models/questionModel.js
@@ -4,6 +4,10 @@ const questionSchema = new mongoose.Schema({
     question: String,
     answers: [
     ],
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'stackoverflowUser'
+    }
 });
 
 module.exports = mongoose.model("Question", questionSchema);
