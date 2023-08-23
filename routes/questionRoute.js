960a7c2fcb0e251c29074144e5751cca73a1d1f2
steven-mpawulo <stevenmpawulo@gmail.com>
2023-08-23 10:25:23 +0300

    adds sign up controller to router

--- routes/questionRoute.js
+++ routes/questionRoute.js
@@ -5,6 +5,7 @@ const getAllQuestions = require("../controllers/getAllQuestionsController");
 const getSpecificQuestion = require("../controllers/getSpecificQuestionController");
 const addAnswer = require('../controllers/addAnswerController');
 const deleteQuestion = require("../controllers/deleteQuestionController");
+const signup = require("../controllers/signupController");
 const questionRouter = express.Router();
 
 questionRouter.post('/questions', addQuestion);
@@ -12,6 +13,7 @@ questionRouter.get('/questions', getAllQuestions);
 questionRouter.get('/questions/:id', getSpecificQuestion);
 questionRouter.get('/questions/:id/answers', addAnswer);
 questionRouter.delete('/questions/:id', deleteQuestion);
+questionRouter.post('/signup', signup);
 
 
 module.exports = questionRouter;
