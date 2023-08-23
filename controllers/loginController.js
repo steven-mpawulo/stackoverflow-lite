46d2499d5f16751118048cc446c3894f6947c787
steven-mpawulo <stevenmpawulo@gmail.com>
2023-08-23 11:35:57 +0300

    finishes creating tokens after login

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const userModel = require('../models/userModel');
 const login = (req, res) => {
     const body = req.body;
@@ -9,7 +10,10 @@ const login = (req, res) => {
                 console.log(value);
                 const result = await bcrypt.compare(body.password, value.password);
                 if (result){
-                    res.status(200).json({"message": "user login successful"});
+                    const token = jwt.sign({'_id': value._id}, process.env.SECRETKEY, {
+                        expiresIn: 60 * 60 * 60
+                    });
+                    res.status(200).json({"message": "user login successful", "token": token});
                 } else {
                     res.status(400).json({"message": "user login failed"});
                 }
