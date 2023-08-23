3761ec61ed2a36805ff6f888f6e68ac6108d0d82
steven-mpawulo <stevenmpawulo@gmail.com>
2023-08-23 10:54:45 +0300

    finishes up on login controller

---
+++ controllers/loginController.js
@@ -0,0 +1,28 @@
+const bcrypt = require('bcrypt');
+const userModel = require('../models/userModel');
+const login = (req, res) => {
+    const body = req.body;
+    console.log(body);
+    if (Object.keys(body).length !== 0){
+        if (body.email && body.password){
+            userModel.findOne({"email": body.email}).then(async (value) => {
+                console.log(value);
+                const result = await bcrypt.compare(body.password, value.password);
+                if (result){
+                    res.status(200).json({"message": "user login successful"});
+                } else {
+                    res.status(400).json({"message": "user login failed"});
+                }
+            }).catch((e) => {
+                console.log(e);
+                res.status(400).json({"message": "something went wrong"});
+            });
+        }
+
+    } else {
+        res.status(400).json({"message": "something went wrong"});
+    }
+
+}
+
+module.exports = login;
