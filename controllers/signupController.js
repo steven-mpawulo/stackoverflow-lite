d36ce3f70feb9108bb7f4928ee1b712324601a87
steven-mpawulo <stevenmpawulo@gmail.com>
2023-08-23 11:38:01 +0300

    creates token after sign up

--- controllers/signupController.js
+++ controllers/signupController.js
@@ -1,5 +1,6 @@
 const bcrypt = require('bcrypt');
 const userModel = require('../models/userModel');
+const jwt = require('jsonwebtoken');
 const signup = async (req, res) => {
     const body = req.body;
     if (Object.keys(body).length === 0) {
@@ -15,9 +16,13 @@ const signup = async (req, res) => {
 
         await user.save().then((value) => {
             console.log(value);
+            const token = jwt.sign({'_id': value._id}, process.env.SECRETKEY, {
+                expiresIn: 60 * 60 * 60
+            });
             res.status(200).json({
                 "message": "accounted created",
                 "user details":value,
+                "token": token
             });
         }).catch((e) => {
             console.log(e);
