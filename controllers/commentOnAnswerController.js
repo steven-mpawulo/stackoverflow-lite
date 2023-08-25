7fae3a202b7d65d3cbebf38a128e752c37598ce1
steven-mpawulo <stevenmpawulo@gmail.com>
2023-08-25 11:30:02 +0300

    finishes adding comment on answer

--- controllers/commentOnAnswerController.js
+++ controllers/commentOnAnswerController.js
@@ -5,16 +5,24 @@ const commentOnAnswer = (req, res) => {
     console.log(questionId);
     const answerId = req.params.id2;
     console.log(answerId);
-    const newAnswerId = mongoose.Types.ObjectId(answerId);
+    const newAnswerId = new mongoose.Types.ObjectId(answerId);
     const body = req.body;
     console.log(body);
 
     if (questionId !== null && answerId !== null) {
         if (Object.keys(body).length !== 0){
-            questionModel.findOneAndUpdate({"answers._id": newAnswerId}, {'$set': {'answers.$.comment': body.comment}}).then((value) => {
-                console.log(value);
-                res.status(200).json({"message": "comment added", "question": value});
-            }).catch((e) => {});
+            if (body.comment){
+                questionModel.findOneAndUpdate({"answers._id": newAnswerId}, {'$set': {'answers.$.comment': body.comment}}, {new: true}).then((value) => {
+                    console.log(value);
+                    res.status(200).json({"message": "comment added", "question": value});
+                }).catch((e) => {
+                    console.log(e);
+                    res.status(400).json({"message": "failed to add comment"});
+                });
+            } else {
+                res.status(400).json({"message": "something went wrong"});
+            }
+            
         } else {
             res.status(400).json({"message": "something went wrong"});
         }
