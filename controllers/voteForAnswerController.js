47aeb1ee4187de7a6226118e5f27796ca80ba489
steven-mpawulo <stevenmpawulo@gmail.com>
2023-08-25 11:56:24 +0300

    works on increasing and decreasing votes

--- controllers/voteForAnswerController.js
+++ controllers/voteForAnswerController.js
@@ -4,16 +4,25 @@ const voteForAnswer = async (req, res) => {
     const questionId = req.params.id;
     const answerId = req.params.id2;
     const newAnswerId = new mongoose.Types.ObjectId(answerId);
+    const body = req.body;
 
 
     if (questionId !== null && answerId !== null) {
-        await questionModel.findOneAndUpdate({ "answers._id": newAnswerId }, { '$inc': {"answers.$.votes": 1} }, {new: true}).then((value) => {
-            console.log(value);
-            res.status(200).json({ "message": "answer updated", "question": value });
-        }).catch((e) => {
-            console.log(e);
-            res.status(400).json({ "message": "failed to update answer" });
-        });
+        if (Object.keys(body).length !== 0){
+            const increment = req.body.upvote ? true : false;
+            console.log(increment);
+            await questionModel.findOneAndUpdate({ "answers._id": newAnswerId }, increment ? { '$inc': {"answers.$.votes": 1} } : { '$inc': {"answers.$.votes": -1} }, {new: true}).then((value) => {
+                console.log(value);
+                res.status(200).json({ "message": "answer updated", "question": value });
+            }).catch((e) => {
+                console.log(e);
+                res.status(400).json({ "message": "failed to update answer" });
+            });
+
+        } else {
+            res.status(400).json({"message": "somethin went wrong"});
+        }
+        
     }
 }
 
